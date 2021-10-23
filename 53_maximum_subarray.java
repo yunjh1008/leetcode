7cd41fe4aa305261461cb9732c07bb3e9d0d1ad9
yunjh1008 <yunjh1008@gmail.com>
2021-10-24 00:37:13 +0900

    Update 53_maximum_subarray.java

--- 53_maximum_subarray.java
+++ 53_maximum_subarray.java
@@ -2,7 +2,7 @@
 class Solution {
     public int maxSubArray(int[] nums) {
         // Get maximum sub array sum of the given array in O(n) by using Kadane's Algorithm
-		    // Learn more: https://r4bb1t.tistory.com/10
+	// Learn more: https://r4bb1t.tistory.com/10
         int currentMaxSum = nums[0];
         int maxSum = nums[0];
         
