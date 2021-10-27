9346884d777cfbd846de889fb002112661922285
yunjh1008 <yunjh1008@gmail.com>
2021-10-28 00:23:11 +0900

    Create 32_longest_valid_parenthesis.java

---
+++ 32_longest_valid_parenthesis.java
@@ -0,0 +1,56 @@
+class Solution {
+    // 1. Brute Force - O(n^3)
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '(') {
+                stack.push(s.charAt(i));
+            }
+            else if(!stack.empty() && stack.peek() == '(') {
+                stack.pop();
+            }
+            else {
+                return false;
+            }
+        }
+        return stack.empty();
+    }
+    
+    public int longestValidParenthesesBruteForce(String s) {
+        int maxLen = 0;
+        for(int i = 0; i < s.length(); i++) {
+            for(int j = i+2; j <= s.length(); j++) {
+                if(isValid(s.substring(i, j))) {
+                    maxLen = Math.max(maxLen, j - i);
+                }
+            }
+        }
+        
+        return maxLen;
+    }
+    // Brute Force
+    
+    // Using stack - O(n)
+    public int longestValidParentheses(String s) {
+        int maxLen = 0;
+        Stack<Integer> stack = new Stack<Integer>();
+        stack.push(-1);
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '(') {
+                stack.push(i);
+            }
+            else {
+                stack.pop();
+                if(stack.empty()) {
+                    stack.push(i);
+                }
+                else {
+                    System.out.println("i = " + i + ", stack.peek() = " + stack.peek());
+                    maxLen = Math.max(maxLen, i - stack.peek());
+                }
+            }
+        }
+        
+        return maxLen;
+    }
+}
