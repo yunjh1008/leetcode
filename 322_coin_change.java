e070cc7e37b4f9ec661e5265ee1fe7cfaa07e9fd
yunjh1008 <yunjh1008@gmail.com>
2021-11-06 21:43:11 +0900

    Create 322_coin_change.java

---
+++ 322_coin_change.java
@@ -0,0 +1,18 @@
+// 322. Coin Change
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        
+        for(int i = 1; i <= amount; i++) {
+            for(int j = 0; j < coins.length; j++) {
+                // amount >= denomination of coin
+                if(i >= coins[j]) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+        
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
+    }
